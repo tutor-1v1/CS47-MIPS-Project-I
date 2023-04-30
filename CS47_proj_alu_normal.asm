9eae8de229b607bc3879707d681a7ff041b4dc2a
tutor-1v1 <liumeng64@gmail.com>
2023-04-30 16:03:30 +0800

    WeChat cstutorcs CS47 MIPS Project I

---
+++ CS47_proj_alu_normal.asm
@@ -0,0 +1,23 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+.include "./cs47_proj_macro.asm"
+.text
+.globl au_normal
+# TBD: Complete your project procedures
+# Needed skeleton is given
+#####################################################################
+# Implement au_normal
+# Argument:
+# 	$a0: First number
+#	$a1: Second number
+#	$a2: operation code ('+':add, '-':sub, '*':mul, '/':div)
+# Return:
+#	$v0: ($a0+$a1) | ($a0-$a1) | ($a0*$a1):LO | ($a0 / $a1)
+# 	$v1: ($a0 * $a1):HI | ($a0 % $a1)
+# Notes:
+#####################################################################
+au_normal:
+# TBD: Complete it
+	jr	$ra
---
+++ cs47_proj_macro.asm
@@ -0,0 +1,7 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+# Add you macro definition here - do not touch cs47_common_macro.asm"
+#<------------------ MACRO DEFINITIONS ---------------------->#
+
